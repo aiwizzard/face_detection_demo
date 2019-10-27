0d1deb99bdbfa5bd218ad10fd5d84e6d31b6d1e0
Ajmal K <ajmalekvsroad@gmail.com>
2019-10-27 22:15:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,23 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    This is the view for registering a user.
+    """
+    
     form = RegistrationForm()
+    
+    # if the current user is authenticated then redirected to the home page
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     if form.validate_on_submit():
+        # encrypt the password using bcrypt
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         if form.picture.data:
+            # the save_picture function from the utils module for saving the image in cropped size
             image_file = save_picture(form.picture.data)
+            
+        # setting the user with the data from the register form.
         user = User(username=form.username.data, email=form.email.data, 
                     password=hashed_password, image_file=image_file)
         db.session.add(user)
@@ -31,9 +41,16 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    this is the view for siginig in a user
+    """
+  
+    # if the current user is authenticated then redirect to the home page.
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = LoginForm()
+    
+    # validating the data from the form with the data from the database.
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first() 
         if user and bcrypt.check_password_hash(user.password, form.password.data):
@@ -46,6 +63,11 @@ def login():
 
 @app.route('/logout')
 def logout():
+    """
+    This is the view for logout
+    """
+    
+    # using the method from the flask module
     logout_user()
     return redirect(url_for('home'))
 
@@ -53,17 +75,27 @@ def logout():
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    """
+    This is the view for updating the account information of the authenticated user
+    """
+    
     form = UpdateAccountForm()
+    
+    # perform actions when the form is submitted
     if form.validate_on_submit():
+        # checking if the form contains a picture file
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
+        # changing the current user details with the form data
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
+    # performs action if the form method is get
     elif request.method == 'GET':
+        # setting the form data with the user data from the database
         form.username.data = current_user.username
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
