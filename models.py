9402595cf2769c486933edc0b747fae647e3e4d8
Ajmal K <ajmalekvsroad@gmail.com>
2019-10-27 22:26:32 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,6 +6,9 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+    """
+    table for the user model
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
